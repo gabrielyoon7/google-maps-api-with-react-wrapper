4a73c62fe73dd020b2e9dbda3ead454a9ae98618
gabrielyoon7 <gabrielyoon7@gmail.com>
2023-11-09 00:15:34 +0900

    chore: tanstack query 기본 설치 및 환경설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 async function deferRender() {
   if (process.env.NODE_ENV !== 'development') {
@@ -12,10 +14,15 @@ async function deferRender() {
   return worker.start()
 }
 
+const queryClient = new QueryClient()
+
 deferRender().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </React.StrictMode>,
   )
 })
